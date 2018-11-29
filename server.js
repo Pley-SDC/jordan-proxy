6efaf78976fa32a4f0dfcde59f84ae7e4d9b6cf0
Jordan Holmes <jordanstholmes@gmail.com>
2018-11-28 16:46:51 -0800

    Comment out unused routes

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ const overviewUrl = process.env.OVERVIEW_URL || 'http://localhost:9001/';
 // app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/:id', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/public/index.html'));
-// })
+app.get('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+})
 
-app.get('/overview/restaurants/:id', proxy({
-  target: overviewUrl
-}))
+// app.get('/overview/restaurants/:id', proxy({
+//   target: overviewUrl
+// }))
 
-app.post('/overview/restaurants/:restaurantId/images/', proxy({
-  target: overviewUrl
-}))
+// app.post('/overview/restaurants/:restaurantId/images/', proxy({
+//   target: overviewUrl
+// }))
 
 app.listen(port, () => {
   console.log(`server running on ${port}`);
