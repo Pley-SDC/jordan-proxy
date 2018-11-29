2389acc683336bb592fa87788204531d055bfc3e
Jordan Holmes <jordanstholmes@gmail.com>
2018-11-28 19:13:44 -0800

    Fix reservation routing

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const overviewUrl = process.env.OVERVIEW_URL || 'http://localhost:9001';
 const reservationUrl = process.env.RESERVATION_URL || "http://ec2-3-16-181-60.us-east-2.compute.amazonaws.com:3002";
+const reservationProxy = proxy({target: reservationUrl});
 
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('short'));
@@ -20,9 +21,8 @@ app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
-app.use('*', proxy({
-  target: reservationUrl
-}))
+app.get('/api/restaurant_id/reservation', reservationProxy);
+app.get('/api/restaurant_id/hour', reservationProxy);
 
 // app.post('/overview/restaurants/:restaurantId/images/', proxy({
 //   target: overviewUrl
