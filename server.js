884a0cf3852e9c9e8511f209316330b74cfc851b
Jordan Holmes <jordanstholmes@gmail.com>
2018-11-30 19:58:54 -0800

    Add additional proxying routes for new services

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const proxy = require('http-proxy-middleware');
 const app = express();
 const port = process.env.PORT || 3000;
 const overviewUrl = process.env.OVERVIEW_URL || 'http://13.52.10.32';
-const reservationUrl = process.env.RESERVATION_URL || "http://ec2-3-16-181-60.us-east-2.compute.amazonaws.com:3002";
+const reservationUrl = process.env.RESERVATION_URL || 'http://ec2-3-16-181-60.us-east-2.compute.amazonaws.com:3002';
+const dishesUrl = process.env.DISHES_URL || 'http://ec2-18-222-177-6.us-east-2.compute.amazonaws.com/';
+const reviewsUrl = process.env.DISHES_URL || 'http://13.52.60.21:3003'
 
 const reservationProxy = proxy({target: reservationUrl});
 const overviewProxy = proxy({target: overviewUrl});
+const dishesProxy = proxy({target: dishesUrl});
+const reviewsProxy = proxy({target: reviewsUrl});
 
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('short'));
@@ -26,6 +30,8 @@ app.get('/:id', (req, res) => {
 app.use('/api/:restaurant_id/reservation', reservationProxy);
 app.use('/api/:restaurant_id/hour', reservationProxy);
 app.use('/overview', overviewProxy);
+app.use('/api/:restaurantId/menu', dishesProxy);
+app.use('/api/:id', reviewsProxy);
 
 // app.post('/overview/restaurants/:restaurantId/images/', proxy({
 //   target: overviewUrl
